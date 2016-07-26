8d1a325fe45fea01c4733fbd688c51d4249f3dba
Trevor Murphy <murphtrevor@gmail.com>
2016-07-26 12:23:56 -0500

    Test if Miranda can echo messages back.

--- app.js
+++ app.js
@@ -1,16 +1,122 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+
+app.listen(process.env.PORT || 5000);
 
 app.get('/', function(req, res) {
-    res.send('The server is OK.');
+	res.send('The server is OK.');
 });
 
 app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === 'miranda_verify') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Error, wrong validation token');
-    }
+	if (req.query['hub.mode'] === 'subscribe' &&
+		req.query['hub.verify_token'] === 'miranda_verify') {
+		console.log("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error("Failed validation. Make sure the validation tokens match.");
+		res.sendStatus(403);
+	}
 });
 
-app.listen(process.env.PORT || 5000);
+app.post('/webhook', function(req, res) {
+	var data = req.body;
+
+	// Make sure this is a page subscription
+	if (data.object == 'page') {
+		// Iterate over each entry
+		// There may be multiple if batched
+		data.entry.forEach(function(pageEntry) {
+			var pageID = pageEntry.id;
+			var timeOfEvent = pageEntry.time;
+
+			// Iterate over each messaging event
+			pageEntry.messaging.forEach(function(messagingEvent) {
+				if (messagingEvent.optin) {
+					receivedAuthentication(messagingEvent);
+				} else if (messagingEvent.message) {
+					receivedMessage(messagingEvent);
+				} else if (messagingEvent.delivery) {
+					receivedDeliveryConfirmation(messagingEvent);
+				} else if (messagingEvent.postback) {
+					receivedPostback(messagingEvent);
+				} else {
+					console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+				}
+			});
+		});
+	}
+});
+
+function receivedMessage(event) {
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
+
+	console.log("Received message for user %d and page %d at %d with message:",
+		senderId, recipientID, timeOfMessage);
+	console.log(JSON.stringify(message));
+
+	var messageId = message.mid;
+
+	// You may get a text or attachment but not both
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+
+	if (messageText) {
+		switch (messageText) {
+			case 'image':
+				sendImageMessage(senderID);
+				break;
+			case 'button':
+				sendButtonMessage(senderID);
+				break;
+			case 'generic':
+				sendGenericMessage(senderID);
+				break;
+			case 'receipt':
+				sendReceiptMessage(senderID);
+				break;
+			default:
+				sendTextMessage(senderID, messageText);
+		}
+	} else if (messageAttachments) {
+		sendTextMessage(senderID, "Message with attachment received");
+	}
+}
+
+function sendTextMessage(recipientId, messageText) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: messageText
+		}
+	};
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/mesages',
+		qs: {
+			access_token: process.env.PAGE_ACCESS_TOKEN
+		},
+		method: 'POST',
+		json: messageData
+	}, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			console.log("Successfully sent generic message with id %s to" +
+				" recipient % s", messageId, recipientId);
+		} else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+	});
+}
