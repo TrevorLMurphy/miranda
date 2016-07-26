d107d60dbfaec1d6bff7a6d9c8038c23d81f4eb7
Trevor Murphy <murphtrevor@gmail.com>
2016-07-26 13:53:56 -0500

    Add a temporary pizza response

--- brain.js
+++ brain.js
@@ -7,7 +7,9 @@ module.exports = {
             " to meet you :)";
         if (hellos.indexOf(chat) > -1) {
             return greeting;
+        } else if (chat === 'pizza') {
+            return "Trevor is soon going to teach me how to order pizza.";
         }
-        return "I'm sorry, I'm very young and don't understand what you're saying :("
+        return "I'm sorry, I'm very young and don't understand what you're saying :(";
     }
 };
