4204167271cfaa8f9e6a973693de4b76c7a3f403
Trevor Murphy <murphtrevor@gmail.com>
2016-07-26 10:41:51 -0500

    Add starting files

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello', function (req, res) {
+    res.send('world!');
+});
+
+app.get('/webhook', function (req, res) {
+    if (req.query(['hub.verify_token'] === 'secret-token')) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong validation token');
+    }
+});
+
+app.listen(process.env.PORT);
