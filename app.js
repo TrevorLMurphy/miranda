6638e3feebbe8307e1a2fd3223ea2711bb6306b4
Trevor Murphy <murphtrevor@gmail.com>
2016-08-01 08:20:17 -0500

    Update code structure and add more functionality
    The structure of the code has been altered to better
    resemble Facebook's own example bot. The use of the function
    'callSendApi' is cleaner and will allow better extension
    into creating messages that aren't just text.
    
    RetrieveUserInfo is a function to add a more personal touch
    to Miranda. However, due to async programming, this was a lot
    harder than I anticipated. I used an example from someone else
    that involved utilizing callbacks, however I'm still not entirely
    familiar with the concept and need to keep investigating.
    
    Finally, changed how Miranda identified typical greetings by
    using regex. This is extremely dumb and will be changed in the
    future.

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.get('/', function(req, res) {
 app.get('/webhook', function(req, res) {
 	if (req.query['hub.mode'] === 'subscribe' &&
 		req.query['hub.verify_token'] === 'miranda_verify') {
-		console.log("Validating webhook");
+		console.log("Validating webhook...");
 		res.status(200).send(req.query['hub.challenge']);
+		console.log("Webhook validated");
 	} else {
 		console.error("Failed validation. Make sure the validation tokens match.");
 		res.sendStatus(403);
@@ -29,35 +30,79 @@ app.post('/webhook', function(req, res) {
 	var events = req.body.entry[0].messaging;
 	for (i = 0; i < events.length; i++) {
 		var event = events[i];
-		if (event.message && event.message.text) {
-            var greeting = brain.think(event.message.text);
-            sendMessage(event.sender.id, greeting);
+		if (event.message && event.message.text && !event.message.is_echo) {
+			retrieveUserInfo(event.sender.id, function(err, profile) {
+				if (err) throw err;
+				response = brain.think(event.message.text, profile);
+				sendMessage(event.sender.id, response);
+			});
 		}
 	}
 	res.sendStatus(200);
 });
 
 // generic function sending messages
-function sendMessage(recipientId, message) {
+function sendMessage(recipientId, messageText) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: messageText,
+		}
+	};
+	callSendAPI(messageData);
+}
+
+function sendSticker(recipientId) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			attachment: {
+				type: "image",
+				payload: {
+					"url": "https://scontent.xx.fbcdn.net/t39.1997-6/p100x100/11405203_1457726111188768_768486346_n.png?_nc_ad=z-m"
+				}
+			}
+		}
+	};
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
 	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {
 			access_token: process.env.PAGE_ACCESS_TOKEN
 		},
 		method: 'POST',
-		json: {
-			recipient: {
-				id: recipientId
-			},
-			message: {
-				text: message
-			},
-		}
+		json: messageData
 	}, function(error, response, body) {
 		if (error) {
-			console.log('Error sending message: ', error);
+			return console.log('Error sending message: ', error);
 		} else if (response.body.error) {
-			console.log('Error: ', response.body.error);
+			return console.log('Error: ', response.body.error);
 		}
+	});
+}
+
+function retrieveUserInfo(userId, callback) {
+	if (!callback) callback = Function.prototype;
+	request({
+		uri: 'https://graph.facebook.com/v2.6/' + userId.toString(),
+		qs: {
+			access_token: process.env.PAGE_ACCESS_TOKEN
+		},
+		method: 'GET',
+		json: true
+	}, function(error, response, body) {
+		if (error) {
+			return console.log('Error retrieving user info: ', error);
+		} else if (response.body.error) {
+			return console.log('Error: ', response.body.error);
+		}
+		callback(null, body);
 	});
 }
--- brain.js
+++ brain.js
@@ -1,15 +1,18 @@
 //=======Miranda's Brain=========//
 
+var util = require('util');
+
 module.exports = {
-    think: function(chat) {
-        var hellos = ['hello', 'hi', 'hey'];
-        var greeting = "Hello there! My name is Miranda. It's a pleasure" +
-            " to meet you :)";
-        if (hellos.indexOf(chat) > -1) {
-            return greeting;
-        } else if (chat === 'pizza') {
-            return "Trevor is soon going to teach me how to order pizza.";
+	think: function(chat, user) {
+		var hellos = /(hey|hi|hello)\b/i;
+		var greeting = util.format("Hello %s! My name is Miranda. It's a " +
+			"pleasure to meet you :)", user.first_name);
+		if (hellos.exec(chat) !== null) {
+			return greeting;
+		} else if (chat === 'pizza') {
+			return "Trevor is soon going to teach me how to order pizza.";
+		} else {
+            return "I'm sorry, I'm very young and don't understand what you're saying :(";
         }
-        return "I'm sorry, I'm very young and don't understand what you're saying :(";
-    }
+	}
 };
